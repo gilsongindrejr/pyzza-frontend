3844ee6fe5bda00c18819de57bd99fe0899b2e3c
Gilson Gindre Jr <gindrejunior@hotmail.com>
2022-10-23 23:16:49 -0300

    feat: finished navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,45 @@
 import styles from './Navbar.module.css';
 
+import { Link } from 'react-router-dom';
+
+import logo from '../pyzza.png'
+
+import Search from './Search';
+import Categories from './Categories';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCategories } from '../state/features/categorySlice';
+
 const Navbar = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [])
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.logo}>
+                    <Link to="#"><img src={logo} alt="logo" /></Link>
+                </div>
+                <div className={styles.buttons}>
+                    <div className={styles.buttons_container}>
+                        <Link to="#" className='btn btn_white'>Register</Link>
+                        <Link to="#" className='btn btn_crimson'>Login</Link>
+                    </div>
+                </div>
+                <div className={styles.search}>
+                    <Search />
+                </div>
+            </nav>
+            <div className={styles.categories}>
+                <div>
+                    <Categories />
+                </div>
+            </div>
+        </>
     )
 }
 
