b63f826e4eb38868e9389f9b6282fc112d8cd9ae
Gilson Gindre Jr <gindrejunior@hotmail.com>
2022-10-23 23:18:58 -0300

    feat: added navbar and created paths using react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <div className="container">
+        <BrowserRouter>
+          <Navbar />
+          {/* Add routes here */}
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
