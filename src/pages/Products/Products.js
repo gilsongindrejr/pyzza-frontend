977dd2bf60f3e3dea726ce174c00d39220dc684c
GilsonJr <gindrejunior@hotmail.com>
2022-11-05 14:14:49 -0300

    feat: created products page

---
+++ src/pages/Products/Products.js
@@ -0,0 +1,45 @@
+import styles from './Products.module.css';
+
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+
+import ProductCard from '../../components/ProductCard';
+import { getProducts } from '../../state/features/productSlice';
+
+const Products = () => {
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const search = searchParams.get('search');
+    const filter = searchParams.get('filter');
+    const params = {}
+
+    // set param only if he exist
+    // that way the length of the param can be used
+    search && (params.search = search)
+    filter && (params.filter = filter)
+
+    const {products, loading} = useSelector(state => state.product);
+
+    useEffect(() => {
+        dispatch(getProducts(params))
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            {products && products.map(product => (
+                <ProductCard key={product.id} image={product.image} name={product.name} desc={product.description} price={product.price} />
+            ))}
+            {products.length === 0 && <h1 className={styles.no_products}>Nao foram encontrados produtos</h1>}
+            {loading && <h1>Loading...</h1>}
+        </div>
+    )
+}
+
+export default Products
