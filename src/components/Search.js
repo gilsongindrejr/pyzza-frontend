ae542b02dc980499648eca36b12dced9fca9c63b
GilsonJr <gindrejunior@hotmail.com>
2022-11-05 17:02:49 -0300

    feat: added search feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,46 +13,56 @@ import { Link } from 'react-router-dom';
 const Search = () => {
     const { categories, loading } = useSelector(state => state.category)
 
-    const [selectedCategory, setSelectedCategory] = useState('Category');
+    const [filter, setFilter] = useState('Category');
+
+    const [search, setSearch] = useState('');
 
     const [isActive, setIsActive] = useState(false);
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
 
     const handleDropdown = (e) => {
         e.preventDefault();
         setIsActive(!isActive);
     }
 
-    const handleSelectedCategory = (e) => {
+    const handleFilter = (e) => {
         e.preventDefault();
-        setSelectedCategory(e.target.value);
+        setFilter(e.target.value);
         setIsActive(!isActive);
+    }
+
+    const handleSubmit = () => {
+        if(filter === 'Category' || filter === 'All'){
+            setFilter('')
+        }
     }
 
     return (
         <div className={styles.wrap}>
-            <form action="" className={styles.form}>
-                <div className={styles.form_container}>
-                    <div className={styles.form_category}>
+                <div className={styles.container}>
+                    <div className={styles.category}>
                         <div className={styles.dropdown}>
-                            <button className={styles.dropdown_btn} onClick={handleDropdown}>{selectedCategory} <FontAwesomeIcon icon={faCaretDown} /></button>
+                            <button className={styles.dropdown_btn} onClick={handleDropdown}>{filter ? filter : 'All'} <FontAwesomeIcon icon={faCaretDown} /></button>
                             <div className={isActive ? styles.dropdown_content : ''}>
                                 {isActive && loading && <input type="button" className={styles.dropdown_link} value="Loading..." />}
-                                {isActive && !loading && <input type="button" className={styles.dropdown_link} onClick={handleSelectedCategory} value="All" />}
+                                {isActive && !loading && <input type="button" className={styles.dropdown_link} onClick={handleFilter} value="All" />}
                                 {isActive && !loading &&
                                     categories.map(category =>
-                                        <input key={category.id} className={styles.dropdown_link} onClick={handleSelectedCategory} value={category.category} />
+                                        <input key={category.id} className={styles.dropdown_link} onClick={handleFilter} defaultValue={category.name} />
                                     )}
                             </div>
                         </div>
                     </div>
-                    <div className={styles.form_input}>
-                        <input type="text" placeholder='Search' />
+                    <div className={styles.input_container}>
+                        <input type="text" placeholder='Search' value={search} onChange={handleSearch} />
                     </div>
-                    <div className={styles.form_btn}>
-                        <button type='submit' className={styles.search_btn}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                    <div className={styles.btn_container}>
+                        <a href={filter ? `products?search=${search}&filter=${filter}` : `products?search=${search}`} className={styles.search_btn} onClick={handleSubmit}><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
                     </div>
                 </div>
-            </form>
         </div>
     )
 }
