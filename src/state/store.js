80c4ce9e697618022184c0bfa9f5ac0932dda25f
GilsonJr <gindrejunior@hotmail.com>
2022-11-10 15:01:45 -0300

    feat: register login reducer in store

--- src/state/store.js
+++ src/state/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import categoryReducer from './features/categorySlice';
 import productReducer from './features/productSlice';
+import loginReducer from './features/loginSlice';
 
 export default configureStore({
     reducer: {
         category: categoryReducer,
         product: productReducer,
+        auth: loginReducer,
     }
 })
