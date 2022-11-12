b04a70a498cb8877a9b7c7feef3848f61e497cf3
GilsonJr <gindrejunior@hotmail.com>
2022-11-11 22:43:18 -0300

    feat: used redux-persist to persist user info after refresh

--- src/state/store.js
+++ src/state/store.js
@@ -1,13 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from "redux-thunk";
+
 import categoryReducer from './features/categorySlice';
 import productReducer from './features/productSlice';
 import loginReducer from './features/loginSlice';
 
-export default configureStore({
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedLoginReducer = persistReducer(persistConfig, loginReducer)
+
+export const store = configureStore({
     reducer: {
         category: categoryReducer,
         product: productReducer,
-        auth: loginReducer,
-    }
+        auth: persistedLoginReducer,
+    },
+    middleware: [thunk]
 })
+
+export const persistor = persistStore(store)
